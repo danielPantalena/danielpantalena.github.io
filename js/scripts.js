9805d5755cda053fd3ea97b27baa7711941cdf59
danielpantalena <60634194+danielPantalena@users.noreply.github.com>
2020-03-23 11:26:52 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,22 @@
 //     * Copyright 2013-2020 Start Bootstrap
 //     * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-creative/blob/master/LICENSE)
 //     */
-//     (function($) {
-//   "use strict"; // Start of use strict
+    (function($) {
+  "use strict"; // Start of use strict
 
-//   // Smooth scrolling using jQuery easing
-//   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-//     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-//       var target = $(this.hash);
-//       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//       if (target.length) {
-//         $('html, body').animate({
-//           scrollTop: (target.offset().top - 72)
-//         }, 1000, "easeInOutExpo");
-//         return false;
-//       }
-//     }
-//   });
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 72)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
 
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
