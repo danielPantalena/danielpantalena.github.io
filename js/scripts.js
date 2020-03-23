f831a8a8804bfc3a21bfd9cfb36e83c5adcbf248
danielpantalena <60634194+danielPantalena@users.noreply.github.com>
2020-03-23 11:28:41 -0300

    Chage Js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-// /*!
-//     * Start Bootstrap - Creative v6.0.0 (https://startbootstrap.com/themes/creative)
-//     * Copyright 2013-2020 Start Bootstrap
-//     * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-creative/blob/master/LICENSE)
-//     */
+/*!
+    * Start Bootstrap - Creative v6.0.0 (https://startbootstrap.com/themes/creative)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-creative/blob/master/LICENSE)
+    */
     (function($) {
   "use strict"; // Start of use strict
 
